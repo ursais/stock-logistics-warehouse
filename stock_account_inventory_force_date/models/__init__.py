42052b799b286603f2b7f6f745d6714b1ce4f02c
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-10-13 17:36:23 +0000

    Fix for ticket 67314: Submodule update
    Automated changes for ticket resolution

--- stock_account_inventory_force_date/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_company
-from . import stock_inventory
-from . import stock_inventory_line
-from . import stock_move
-from . import stock_move_line
--- stock_warehouse_orderpoint_stock_info_unreserved/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-This modules allows to display the availability of stock on hand and unreserved on a reordering rule.
